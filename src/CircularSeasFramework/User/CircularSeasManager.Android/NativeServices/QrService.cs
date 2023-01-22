edd492e83ac775d03795f5f7cc27f97558d1a5ac
laboratoriorma3 <122892679+laboratoriorma3@users.noreply.github.com>
2023-01-22 14:33:28 +0100

    Add files via upload

---
+++ src/CircularSeasFramework/User/CircularSeasManager.Android/NativeServices/QrService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CircularSeasManager.Services;
+using System.Threading.Tasks;
+using ZXing.Mobile;
+
+[assembly: Dependency(typeof(CircularSeasManager.Droid.NativeServices.QrService))]
+namespace CircularSeasManager.Droid.NativeServices
+{
+    public  class QrService : IQrService
+    {
+        public async Task<string> ScanAsync()
+        {
+            var optionsDefault = new MobileBarcodeScanningOptions();
+            var optionsCustom = new MobileBarcodeScanningOptions();
+
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Scan the QR Code of your spool",
+                BottomText = "Please Wait",
+            };
+
+            var scanResult = await scanner.Scan(optionsCustom);
+            return scanResult?.Text;
+        }
+    }
+}
